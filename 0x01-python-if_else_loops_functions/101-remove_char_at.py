c79cf6f5a654d708bc9306f5e1cd8eb33d1eff26
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-07 00:27:19 +0300

    remove char at i

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    newString = ''
+    result = ''
     for index in range(0, len(str)):
         if n != index:
-            newString += str[index]
-    return newString
+            result += str[index]
+    return result
