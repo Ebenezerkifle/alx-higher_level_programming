8061c78132a292fbe6dfb95c73a0f4faea7ae1fa
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-06 23:30:28 +0300

    char length error fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
-#ASCII 
-# A - Z =======> 65 - 90
-# a - z =======> 97 - 122
-# 97 - 65 = 32 -- the difference
-
 def uppercase(str):
-    for index in range(0, len(str)):
-        if ord(str[index]) >= 97 and ord(str[index]) <= 122:
-            print('{}'.format(chr(ord(str[index]) - 32)), end='')
-        else:
-            print('{}'.format(str[index]), end='')
+    for letter in str:
+        if ord(letter) >= 97 and ord(letter) <= 122:
+            letter=chr(ord(letter)-32)
+        print('{}'.format(letter), end='')
+    print('')
