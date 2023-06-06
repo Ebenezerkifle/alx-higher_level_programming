e16ef661bb8dda4b6bec9d8cb8bc7d6bc6e3aefd
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-06 22:41:26 +0300

    white space error is fixed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ str = f"Last digit of {number} is "
 if number < 0:
     last_digit *= -1
 if last_digit > 5:
-    str=str + f'{last_digit} and is greater than 5'
+    str = str + f'{last_digit} and is greater than 5'
 elif last_digit == 0:
-    str=str + f'{last_digit} and is 0'
+    str = str + f'{last_digit} and is 0'
 else:
-    str=str + f'{last_digit} and is less than 6 and not 0'
+    str = str + f'{last_digit} and is less than 6 and not 0'
 print(str)
