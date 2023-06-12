38f35d60d13baec8f2ef8e9defcfc3da190b8fb4
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-12 22:06:51 +0300

    a number is divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    
+
+    if len(my_list) is 0:
+        return new_list
+
     for i in my_list:
         if i % 2 == 0:
             new_list.append(True)
