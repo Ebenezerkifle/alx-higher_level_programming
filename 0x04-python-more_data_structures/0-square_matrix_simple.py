1b9eea078a5f983152fdd03c12ada0b32b3d8702
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-14 22:26:18 +0300

    square matrix is solved using lamda and map

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[[]]):
-    new_matrix = len(matrix)*[len(matrix[0])*[0]]
-
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            new_matrix[i][j] = matrix[i][j]*matrix[i][j]
-
-    return new_matrix
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
