b48fc480141fa8c95adc4b41222d8d39ef5ad8af
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-14 09:55:51 +0300

    add_0.py is deleted

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""if is used to block the execusion with import."""
+# 2-args.py
 if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
-    list = sys.argv
-    count = len(list - 1)
-    arg = ''
-    if(count == 1):
-        arg = 'argument:'
-    elif count == 0:
-        arg = 'arguments.'
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        arg = 'arguments:'
-
-    print('{} {}'.format(count, arg))
-
+        print("{:d} arguments:".format(count))
     for i in range(count):
-        print('{}: {}'.format(i + 1, list[i + 1]))
+        print("{:d}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/add_0.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def add(a, b):
-    """The function to add two values
-    """
-    return (a + b)
