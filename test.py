f11dab51917fdfd372938c356feb6996b9da2b48
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-15 16:16:45 +0300

    new directory added and printing a linked list items is solved

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
-def my_function(counter=10):
-    return counter + 1
+def my_func(x,y):
+    return x*y
 
-print(my_function())
-a = { 'id': 89, 'name': "John", 'projects': [1, 2, 3, 4], 'friends': [ { 'id': 82, 'name': "Bob" }, { 'id': 83, 'name': "Amy" } ] }
-b=a.get('friends')[-1]["name"]
-print (b)
+#a=list(map(my_func, [4,6]))
+# def lamda():
+ #   my_list = [5,3,2,1]
+  #  result = (list(map(lambda x , y : x*y , my_list, [5,2,8,0])))
+   # print(result)
+
+def square_matrix_simple(matrix=[[]]):
+    print(matrix)
+    result = [list(map(lambda x: x*x , row)) for row in matrix]
+    return result
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
