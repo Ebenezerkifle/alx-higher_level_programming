9eb30f031dd32beee38ab2e1ba69f443ff3dc38c
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-26 15:31:22 +0300

    safe print of integers from a list which contains any type is added

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            num = num + 1
+        except IndexError:
+            print()
+            return num
+        except:
+            continue
+    print()
+    return num
