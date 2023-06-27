fc4f9325728edc6976d6bc637ae1366dca24b65e
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-27 15:33:12 +0300

    setter and getter methods are added

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+# 4-square.py
+# Abeniezer Kifle
+"""Define a class Square."""
+
 class Square:
-    def __init__(self, size=0):
-           if type(size) is not int:
+    """A Square is a rectangle that have
+    an equale length of the height and width"""
+    def __init__(self, size=0):    
+           if not isinstance(size, int):
                raise TypeError('size must be an integer')
            elif size < 0:
                 raise ValueError('size must be >= 0')
-           else:
-                self.__size = size
+           self.__size = size
 
     def area(self):
          return self.__size * self.__size
 
+    @property
     def size(self):
          return self.__size
 
+    @size.setter
     def size(self, value):
         if type(value) is not int:
                raise TypeError('size must be an integer')
@@ -21,15 +28,3 @@ class Square:
             raise ValueError('size must be >= 0')
         else:
             self.__size = value
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size(3)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
