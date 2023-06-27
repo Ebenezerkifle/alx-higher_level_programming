e5445ff4787de590cde6c972e3c1dbdc902ebbb2
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-27 15:22:27 +0300

    a little fix made

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+# 0-square.py
+# Abeniezer Kifle
+"""Define a class Square."""
+
 class Square:
-    def __init__(self, size=0):
+    """A Square is a rectangle that have
+    an equale length of the height and width"""
+    def __init__(self, size):
+           if type(size) is int:
+                self.__size = size
            if type(size) is not int:
                raise TypeError('size must be an integer')
            elif size < 0:
                 raise ValueError('size must be >= 0')
-           else:
-                self.__size = size
