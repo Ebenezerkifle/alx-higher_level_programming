c3facebb30f4b35f6af9149c039b92a7572657ad
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-27 14:05:02 +0300

    new directory created and square class is added

---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+#Classes in Python
+
+This directory contains the solutions for a problems to be solved using classes in python!
--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+class MyFirstClass:
+    id = 10
+    def __init__(self, name, id):
+        self.name=name
+        MyFirstClass.id = id
+    def getName(self):
+        return self.name
+
 def my_func(x,y):
     return x*y
 
@@ -21,3 +29,9 @@ matrix = [
 new_matrix = square_matrix_simple(matrix)
 print(new_matrix)
 print(matrix)
+
+first = MyFirstClass('Abeni', 50)
+print(first.getName())
+print(first.id)
+print(first.name)
+print(MyFirstClass.id)
