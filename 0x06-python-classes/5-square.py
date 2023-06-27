e5305d2bff33462f4db3c25ca992a2e19f9f3db8
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-27 15:41:17 +0300

    a method to print a # is added

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# 5-square.py
+# Abeniezer Kifle
+"""Define a class Square."""
+
+class Square:
+    """A Square is a rectangle that have
+    an equale length of the height and width"""
+    def __init__(self, size=0):    
+           if not isinstance(size, int):
+               raise TypeError('size must be an integer')
+           elif size < 0:
+                raise ValueError('size must be >= 0')
+           self.__size = size
+
+    def area(self):
+         return self.__size * self.__size
+
+    @property
+    def size(self):
+         return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+               raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def my_print(self):
+        if self.__size is 0:
+             print()
+             return
+
+        for i in range(self.__size):
+            for j in range(self.__size):
+                 print("#", end="")
+            print()
