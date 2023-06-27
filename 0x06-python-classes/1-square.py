5d0995a390d03602fcf0b652c8abdd3d822d0712
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-27 15:17:59 +0300

    init method is added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+# 0-square.py
+# Abeniezer Kifle
+"""Define a class Square."""
+
 class Square:
+    """A Square is a rectangle that have
+    an equale length of the height and width"""
     def __init__(self, size):
-        self.__size = size
+        self.size = size
