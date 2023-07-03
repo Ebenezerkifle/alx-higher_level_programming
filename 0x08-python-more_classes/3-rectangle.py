06e4152ae66520d564527ee9a9d655825fb43c72
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-03 22:27:14 +0300

    a rectangle info on string

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
+# 2-rectangle.py
+# Abeniezer kifle
 """A rectangle class with instance variables height & width
-  & a methods to return an area and perimeter of a rectangle
+and methods area and perimeter....
 """
+
+
 class Rectangle:
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """Initialize a new rectangle
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -16,24 +27,20 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        
         if value < 0:
             raise ValueError('width must be >= 0')
-
         self.__width = value
 
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        
         if value < 0:
             raise ValueError('height must be >= 0')
-
         self.__height = value
 
     def area(self):
@@ -45,6 +52,12 @@ class Rectangle:
         return 2 * (self.__height + self.__width)
 
     def __str__(self):
+        if self.__width is 0 or self.__height is 0:
+            return ("")
+
+        recInfo = []
         for j in range(self.__height):
-            [print("#" , end="") for i in range(self.__width)]
-            print("")
+            [recInfo.append("#") for i in range(self.__width)]
+            if j != self.__height - 1:
+                recInfo.append("\n")
+        return ("".join(recInfo))
