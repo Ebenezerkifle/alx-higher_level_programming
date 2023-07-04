1aa6cd7e67a323dbbb8486603ee3b0a708aa1eed
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-04 17:00:30 +0300

    a function to add two variables is added

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
+#0-add_integer.py
+
+"""A function to add two integers a and b"""
+
+
 def add_integer(a, b=98):
-    if not isinstance(a, int):
+    """Return the sum of two integers a and b
+    
+    Float argumets are typcasted to integer before addition is performed
+
+    Raises:
+        TypeError: If either of the arguments are non-integer or non-float.
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
-    if not isinstance(b, int):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError('b must be an integer')
     return (int(a) + int(b))
-
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, 1))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
