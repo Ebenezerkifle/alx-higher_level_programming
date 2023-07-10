b73c58172075a322d1662ef48afcd578ec30b40f
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-10 22:45:34 +0300

    an empty class is added

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 4-inherits_from.py
+# Abeniezer Kifle
+"""Only Sub class of"""
+
+def inherits_from(obj, a_class):
+    """a function to check if the object is an instance of a class that is inherited
+       from a specified class
+
+       Args:
+         obj: an object
+         a_class: a class
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Abeniezer Kifle
+"""an empty BaseGeometry class"""
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
