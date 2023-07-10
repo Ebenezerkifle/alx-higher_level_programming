a2f20b4df69ab3e45a2ad97bd45b4c1075feeff6
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-10 21:46:17 +0300

    a function which checks if an object is an instance of a class is added

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 2--is_same_class.py
+# Abeniezer Kifle
+"""Defines an object attribute lookup function."""
+
+def is_same_class(obj, a_class):
+    """A function checks is an object is an instance of a class
+    
+    Args:
+      obj: an object which would be checked if it is an instance of a class
+      a_class: a class
+
+    Return:
+        True: if the object is an instance of a class.
+        False: if the object is not an instance of a class.
+    """
+    return type(obj) == a_class
