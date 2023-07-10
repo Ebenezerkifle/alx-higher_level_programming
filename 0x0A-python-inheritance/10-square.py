1ec44399ebccbcd22779e418c4c74d94feb937db
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-10 23:18:10 +0300

    a square class is added

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# 10-square.py
+# Abeniezer Kifle
+"""Full Rectangle class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """A Square class that inherits a Rectangle class"""
+
+    def __init__(self, size):
+        """Intialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        super().integer_validator('Size', size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """the function to return the area of a square"""
+        return self.__size * self.__size
