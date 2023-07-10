60444aadf082ccc1cac5bf77a596787e1079a13e
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-10 22:53:54 +0300

    base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Abeniezer Kifle
+"""an empty BaseGeometry class"""
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented"""
+        raise Exception('area() is not implemented')
