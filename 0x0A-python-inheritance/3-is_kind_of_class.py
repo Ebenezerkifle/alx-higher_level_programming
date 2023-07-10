bb7f18f957f92908fe64997b4b0b7d0f21fe2007
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-11 00:01:10 +0300

    a new line error fixed

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -17,7 +17,6 @@ def is_kind_of_class(obj, a_class):
         True: if the object is an instance of a class.
         False: if the object is not an instance of a class.
     """
-
     if isinstance(obj, a_class):
         return True
     return False
