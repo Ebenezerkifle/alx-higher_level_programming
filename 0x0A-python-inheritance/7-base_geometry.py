60f2c5dd1f11bf5f68d30a088b9361219fbb9769
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-10 23:55:46 +0300

    new line is added

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,6 +3,7 @@
 # Abeniezer Kifle
 """an empty BaseGeometry class"""
 
+
 class BaseGeometry:
     """Represent base geometry."""
 
@@ -21,6 +22,6 @@ class BaseGeometry:
             ValueError: If value is <= 0.
         """
         if not isinstance(value, int):
-            raise TypeError('must be an integer')
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
-            raise ValueError('must be greater than 0')
+            raise ValueError('{} must be greater than 0'.format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -16,6 +16,6 @@ class Rectangle(BaseGeometry):
             height (int): The height of the new Rectangle.
         """
         self.integer_validator('Width', width)
-        self.width = width
+        self.__width = width
         self.integer_validator('Height', height)
-        self.height = height
+        self.__height = height
