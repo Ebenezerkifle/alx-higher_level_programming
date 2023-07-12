bcce05f45c0d98ae05e13dee7afe0f2718f10955
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-13 01:18:17 +0300

    some requirement error fixed

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,11 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """writes an Object to a text file, using a JSON representation"""
+    """writes an Object to a text file, using a JSON representation
+
+       Args:
+          my_obj: an object to be written on a text file.
+          filename: a text file name on which an object is going to be wirte
+    """
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(my_obj, file)
