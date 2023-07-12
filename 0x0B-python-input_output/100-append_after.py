d301a3022e6ad14ffa8ff979dd8cf4a4291c6796
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-12 23:51:51 +0300

    searching a text and adding new string to a file is done

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# 100-append_after.py
+# Abeniezer Kifle
+"""Defining a function that append a line of text"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """A function that appends a line of text after a specific text
+
+       Args:
+           filename: (string) the name of a file
+           search_string: (string) the string on which a new text appends
+           new_string: (string) a string to be appended    
+    """
+    st = ""
+    with open(filename, mode='r', encoding='utf-8') as myFile:
+        while True:
+            line = myFile.readline()
+            if line == "":
+                break
+            st += line
+            if line.__contains__(search_string):
+                st += new_string + "\n"
+
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.write(st)
