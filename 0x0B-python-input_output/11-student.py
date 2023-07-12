2315a7b7d0ff624045c5b62416e0e3aa2b6fbad9
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-12 17:24:10 +0300

    reloading json files to object is done

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+#10-student.py
+# Abeniezer Kifle
+"""Defining a class called Student"""
+
+
+class Student:
+    """A student class
+
+       Args:
+          first_name: (string) first name
+          last_name: (string) last name
+          age: (int) age
+          # which are public instance attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation of a Student instance"""
+        if attrs is not None:
+            res = {key: self.__dict__[key] for key in self.__dict__.keys() & attrs}
+            return res
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """sets an attributes of an object with the values of json"""
+        for key in json.keys():
+            self.__dict__[key] = json[key]
