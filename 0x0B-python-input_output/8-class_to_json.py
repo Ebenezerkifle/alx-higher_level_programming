a9eb7f61a0912ae3c725cc6b2e4f4618dcdfa97c
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-12 17:01:08 +0300

    object to dictionary description for JSON serialiaatioin is added

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#8-class_to_json.py
+# Abeniezer Kifle
+"""Returns the dictionary description for JSON serialization"""
+
+
+def class_to_json(obj):
+    """A function to convert an object to JSON srialization
+       Args: 
+         obj: an object as an input
+       Returns: the directory description for JSON serialization.
+    """
+    return obj.__dict__
