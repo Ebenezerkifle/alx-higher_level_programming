f3954e4eee7a51f6a044a55eaa8b332ec166313e
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-12 16:49:21 +0300

    new line issue is fixed

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,16 +2,11 @@
 #5-save_to_json_file.py
 # Abeniezer Kifle
 """Defins a function that writes an object to a text file"""
+
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """A function to save an object which is converted to JSON
-       representation
-
-       Args: 
-          my_obj: an object to be converted to JSON and saved to a file
-          filename: a file name
-    """
-    with open(filename, mode='w', encoding='utf-8') as myFile:
-        json.dump(my_obj, myFile)
+    """writes an Object to a text file, using a JSON representation"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
