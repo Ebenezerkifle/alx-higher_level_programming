4bc07c1e21e661fcb77f5f718198c017863cf513
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-07-15 16:39:22 +0300

    base class and rectangle class is created

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# rectangle.py
+# Abeniezer Kifle
+"""Defining a Rectangle class."""
+from models.base import Base
+
+class Rectangle(Base):
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super(id)
+        
