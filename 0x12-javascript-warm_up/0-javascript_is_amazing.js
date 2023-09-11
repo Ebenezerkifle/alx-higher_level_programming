f37a0e9063767010fd2ee9ff516f708414aa8ac5
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-09-11 12:21:01 +0300

    javascript is amazing is added with comment

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // This is a javascript script to store a string value and print it on the console.
 
-const myVar = "JavaScript is amazing"
-console.log(myVar)
+const myVar = "JavaScript is amazing";
+console.log(myVar);
