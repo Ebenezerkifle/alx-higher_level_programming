4c33109a1da1da1251f0d165f7b95f0e4153d9ff
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-09-13 01:03:07 +0300

    semi colon added

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const argLength = process.argv.length;
 if(argLength==0){
-    console.log('No argument')
+    console.log('No argument');
 }
 else if(argLength==1){
-    console.log('Argument found')
+    console.log('Argument found');
 }
 else{
-    console.log('Arguments found')
+    console.log('Arguments found');
 }
