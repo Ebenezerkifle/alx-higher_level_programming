3c66109bc500eb161f9fc52300d122bffaf12adf
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-09-13 01:38:03 +0300

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const stringArray = ['C is fun', 'Python is cool','JavaScript is amazing'];
-for(const element of stringArray){
-    console.log(element);
-} 
+for(const element of stringArray) console.log(element);
+
